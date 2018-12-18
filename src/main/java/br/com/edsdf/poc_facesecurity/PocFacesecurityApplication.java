dcf5b531d0d5636c6823944f739c4359e8932afb
Tarcisio Cardoso <tarcardoso@gmail.com>
2018-12-18 15:30:57 -0200

    Construção do upload basico.

--- src/main/java/br/com/edsdf/poc_facesecurity/PocFacesecurityApplication.java
+++ src/main/java/br/com/edsdf/poc_facesecurity/PocFacesecurityApplication.java
@@ -1,9 +1,17 @@
 package br.com.edsdf.poc_facesecurity;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @EnableAutoConfiguration
@@ -17,5 +25,31 @@ public class PocFacesecurityApplication {
 	String home() {
 		return "ola";
 	}
+	
+    @GetMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
+
+    	System.out.println(model);
+    	
+//        model.addAttribute("files", storageService.loadAll().map(
+//                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+//                        "serveFile", path.getFileName().toString()).build().toString())
+//                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+    
+    
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("parameter1") MultipartFile parameter1,
+            RedirectAttributes redirectAttributes) {
+
+//        storageService.store(file);
+        
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
 }
 
